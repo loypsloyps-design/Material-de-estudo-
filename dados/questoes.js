ad1ad9c1e49f055aeee1fda14a5082e23ee291d2
loypsloyps-design <loypsloyps@gmail.com>
2026-08-18 22:14:08 -0300

    Add questions for Língua Portuguesa and Raciocínio Lógico
    Added questions for Língua Portuguesa and Raciocínio Lógico with explanations.

--- dados/questoes.js
+++ dados/questoes.js
@@ -1,1 +1,60 @@
+const questoesPMES = [
 
+    {
+        id: 1,
+
+        materia: "Língua Portuguesa",
+
+        assunto: "Interpretação de Textos",
+
+        enunciado:
+            "A interpretação textual exige que o candidato identifique as informações apresentadas e estabeleça relações entre elas.",
+
+        alternativas: [
+
+            "Apenas informações explícitas.",
+
+            "Somente informações implícitas.",
+
+            "Informações explícitas e implícitas.",
+
+            "Somente a opinião do leitor."
+
+        ],
+
+        resposta: 2,
+
+        explicacao:
+            "A interpretação pode envolver tanto informações diretamente apresentadas quanto informações que podem ser inferidas a partir do texto."
+    },
+
+
+    {
+        id: 2,
+
+        materia: "Raciocínio Lógico",
+
+        assunto: "Proposições",
+
+        enunciado:
+            "Uma proposição lógica é uma sentença que pode ser classificada como verdadeira ou falsa.",
+
+        alternativas: [
+
+            "Verdadeira.",
+
+            "Falsa.",
+
+            "Verdadeira e falsa simultaneamente.",
+
+            "Não pode possuir valor lógico."
+
+        ],
+
+        resposta: 0,
+
+        explicacao:
+            "Uma proposição possui exatamente um valor lógico: verdadeiro ou falso."
+    }
+
+];
