46b3122849a6dd10630c28fc18aee76dc33b2de5
loypsloyps-design <loypsloyps@gmail.com>
2026-08-18 22:12:58 -0300

    Add subjects data to materias.js

--- dados/matérias.js
+++ dados/matérias.js
@@ -1,1 +1,43 @@
+const materiasPMES = [
 
+    {
+        id: 1,
+        nome: "Língua Portuguesa",
+        icone: "📖",
+        cor: "purple",
+        progresso: 18
+    },
+
+    {
+        id: 2,
+        nome: "Raciocínio Lógico",
+        icone: "🧠",
+        cor: "blue",
+        progresso: 12
+    },
+
+    {
+        id: 3,
+        nome: "Geografia",
+        icone: "🌎",
+        cor: "green",
+        progresso: 10
+    },
+
+    {
+        id: 4,
+        nome: "História",
+        icone: "🏛️",
+        cor: "yellow",
+        progresso: 8
+    },
+
+    {
+        id: 5,
+        nome: "Matemática",
+        icone: "🔢",
+        cor: "purple",
+        progresso: 15
+    }
+
+];
