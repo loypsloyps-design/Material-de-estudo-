af1e90638fa589fec31345eefd9e8258096db7fd
loypsloyps-design <loypsloyps@gmail.com>
2026-08-18 22:13:33 -0300

    Add lesson data for multiple subjects
    Added an array of lesson objects for various subjects including details such as title, description, duration, and experience points.

--- dados/aulas.js
+++ dados/aulas.js
@@ -1,1 +1,47 @@
+const aulasPMES = [
 
+    {
+        id: 1,
+        materia: "Língua Portuguesa",
+        titulo: "Interpretação de Textos",
+        descricao:
+            "Compreensão e interpretação de textos verbais e não verbais.",
+        duracao: 10,
+        questoes: 10,
+        xp: 100
+    },
+
+    {
+        id: 2,
+        materia: "Raciocínio Lógico",
+        titulo: "Proposições e Conectivos",
+        descricao:
+            "Introdução às proposições, conectivos e estruturas lógicas.",
+        duracao: 15,
+        questoes: 10,
+        xp: 100
+    },
+
+    {
+        id: 3,
+        materia: "Geografia",
+        titulo: "Localização e Movimentos da Terra",
+        descricao:
+            "Conceitos fundamentais de localização e movimentos terrestres.",
+        duracao: 15,
+        questoes: 10,
+        xp: 100
+    },
+
+    {
+        id: 4,
+        materia: "História",
+        titulo: "Período Colonial do Brasil",
+        descricao:
+            "Principais acontecimentos e características do Brasil Colonial.",
+        duracao: 20,
+        questoes: 10,
+        xp: 100
+    }
+
+];
